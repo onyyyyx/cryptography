1c588b172ba4a7a3bfc1c5f77e38a6aa780173b4
Onyx <shadowangel404@proton.me>
2023-12-19 20:03:09 +0100

    Add files via upload

---
+++ vigenere.py
@@ -0,0 +1,25 @@
+def crypt():
+    alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"*2
+    text=input("Text to crypt :\n> ")
+    crypted,key="",input("Starting key:\n> ")
+    for i in text:
+        if i.islower(): crypted+=alpha[alpha.find(i.upper())+key%len(alpha)].lower()
+        elif i.isupper(): crypted+=alpha[alpha.find(i.upper())+key%len(alpha)].upper()
+        else:
+            crypted+=i
+            key-=1
+        key+=1
+    print("\n",crypted)
+
+def decrypt():
+    alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    crypted=input("Text to decrypt:\n> ")
+    text,key="",input("Starting key:\n> ")
+    for i in text:
+        if i.islower(): text+=alpha[alpha.find(i.upper())+key%len(alpha)].lower()
+        elif i.isupper(): text+=alpha[alpha.find(i.upper())+key%len(alpha)].upper()
+        else:
+            text+=i
+            key-=1
+        key+=1
+    print("\n",crypted)
